8c44af3d6f817e640d38550d94e4353648f48590
6010110345 <55370384+6010110345@users.noreply.github.com>
2019-09-22 12:36:49 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word = "Hello";
+
 class App extends Component {
     render() {
         return (
             <div>
                 {
-                  <WordCard value="hello"/>
+                  <WordCard value="SILANNA"/>
                 }
             </div>
         );
